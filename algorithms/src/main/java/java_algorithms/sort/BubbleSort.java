241caef0b65f668ce3e311f153d635b2f5f6d46b
Stephen Jones <stephenj99642@gmail.com>
2019-05-01 00:14:35 +0100

    Added main() methods back into bubble and insertion sort

--- algorithms/src/main/java/java_algorithms/sort/BubbleSort.java
+++ algorithms/src/main/java/java_algorithms/sort/BubbleSort.java
@@ -1,4 +1,4 @@
-package main.java.java_algorithms.sort;
+package java_algorithms.sort;
 
 import java.util.Arrays;
 
@@ -7,6 +7,23 @@ import java.util.Arrays;
  */
 public class BubbleSort extends SortingAlgorithm
 {
+    public static void main(String[] args)
+    {
+        // Take list from command line and sort it
+        int[] arr = new int[args.length];
+
+        for (int i = 0; i < args.length; i++) {
+            arr[i] = Integer.parseInt(args[i]);
+        }
+
+        arr = (new BubbleSort()).sort(arr);
+
+        // Print results
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
+    }
+
     @Override
     public int[] sort(int[] arr)
     {
--- algorithms/src/main/java/java_algorithms/sort/SortingAlgorithm.java
+++ algorithms/src/main/java/java_algorithms/sort/SortingAlgorithm.java
@@ -1,23 +1,6 @@
-package main.java.java_algorithms.sort;
+package java_algorithms.sort;
 
 public abstract class SortingAlgorithm
 {
-    public static void main(String[] args)
-    {
-        // Take list from command line and sort it
-        int[] arr = new int[args.length];
-
-        for (int i = 0; i < args.length; i++) {
-            arr[i] = Integer.parseInt(args[i]);
-        }
-
-        arr = (new InsertionSort()).sort(arr);
-
-        // Print results
-        for (int i = 0; i < arr.length; i++) {
-            System.out.println(arr[i]);
-        }
-    }
-
-    public abstract int[] sort(int[] arr);
+        public abstract int[] sort(int[] arr);
 }
