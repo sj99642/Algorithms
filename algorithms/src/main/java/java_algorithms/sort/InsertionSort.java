7f7ff38dbdc7880f421f4fb559801cc0f65f8a6b
Stephen Jones <stephenj99642@gmail.com>
2019-05-01 00:21:08 +0100

    Fixed bug and insertion sort now passes tests

--- algorithms/src/main/java/java_algorithms/sort/InsertionSort.java
+++ algorithms/src/main/java/java_algorithms/sort/InsertionSort.java
@@ -30,7 +30,7 @@ public class InsertionSort extends SortingAlgorithm
         // Loop through, expanding the "sorted section" one by one
         for (int numSorted = 1; numSorted < sorted.length; numSorted++) {
             // Get the next item and swap it back until it is in place
-            for (int i = numSorted; i > 1; i--) {
+            for (int i = numSorted; i >= 1; i--) {
                 if (sorted[i] < sorted[i-1]) {
                     temp = sorted[i-1];
                     sorted[i-1] = sorted[i];
